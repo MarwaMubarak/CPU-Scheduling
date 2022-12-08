3a2a9fd6c277326d1b1130c84e9a2b7d35f654c7
Marwa Mubarak <marwamubarak6@gmail.com>
2022-12-08 18:47:31 +0200

    AG

--- src/AG/AG.java
+++ src/AG/AG.java
@@ -1,4 +1,92 @@
 package AG;
 
+import java.util.*;
+
+import Process.Process;
+
 public class AG {
+    private ArrayList<Process> processes=new ArrayList<Process>();
+    private ArrayList<Process> processesOrder=new ArrayList<Process>();
+    private Process currProcess;
+    private HashMap<String,Process>finalProcesses;
+    private int time;
+    private int contextSwitching;
+
+    public AG(ArrayList<Process> processes,int contextSwitching) {
+        this.processes = processes;
+        currProcess=getFirstArrival();
+        time=0;
+        this.contextSwitching=contextSwitching;
+    }
+
+    public void startProcess(){
+        if(processes.isEmpty())
+            return;
+        processesOrder.add(currProcess);
+        if(currProcess.getAG()==1){
+            int firstQuarter=(currProcess.getQuantum()+3)/4;
+            currProcess.setResponseTime(time);
+            int remainingTime= currProcess.getRemainingTime()-firstQuarter;
+            if(remainingTime>0) {
+                time+=firstQuarter;
+                currProcess.setRemainingTime(remainingTime);
+                int newQuantum=currProcess.getQuantum()+2;
+                currProcess.setQuantum(newQuantum);
+                processes.add(currProcess);
+                finalProcesses.replace(currProcess.getProcessName(),currProcess);
+
+            }else if(remainingTime==0){
+                time+=firstQuarter;
+                currProcess.setRemainingTime(remainingTime);
+                processes.add(currProcess);
+                finalProcesses.replace(currProcess.getProcessName(),currProcess);
+            }else{
+
+            }
+
+        }else if(currProcess.getAG()==2){
+
+        }else if(currProcess.getAG()==3){
+
+        }
+        time+=contextSwitching;
+
+
+
+    }
+
+    public Process getFirstArrival(){
+        Process firstArrival=processes.get(0);
+        Iterator<Process> it=processes.iterator();
+        while (it.hasNext()){
+            if(it.next().getArrivalTime()<firstArrival.getArrivalTime()){
+                firstArrival=it.next();
+            }
+        }
+        return firstArrival;
+    }
+
+    public Process getHighestPriority(){
+        Process highestPriority=processes.get(0);
+        Iterator<Process> it=processes.iterator();
+        while (it.hasNext()){
+            if(it.next().getPriority()<highestPriority.getPriority()&&time>=it.next().getArrivalTime()){
+                highestPriority=it.next();
+            }
+        }
+       return highestPriority;
+
+    }
+    public Process getLessRemainingTime(){
+        Process lessTime=processes.get(0);
+        Iterator<Process> it=processes.iterator();
+        while (it.hasNext()){
+            if(it.next().getRemainingTime()<lessTime.getRemainingTime()&&time>=it.next().getArrivalTime()){
+                lessTime=it.next();
+            }
+        }
+        return lessTime;
+
+    }
+
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,8 +1,41 @@
 package Main;
 
+import java.util.*;
+
+import Process.Process;
 public class Main {
 
+
+    static Scanner input=new Scanner(System.in);
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<Process> processes=new ArrayList<Process>();
+        System.out.print("Enter Number Of Process: ");
+        int n = input.nextInt();
+        System.out.print("Enter Round robin Time Quantum: ");
+        int q = input.nextInt();
+        System.out.print("Enter Context switching: ");
+        int context = input.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println("For P" + i );
+
+            System.out.print("Enter process Name: ");
+            String processName = input.next();
+
+            System.out.print("Enter Arrival Time: ");
+            int arrivalTime = input.nextInt();
+
+            System.out.print("Enter Burst Time: ");
+            int burstTime = input.nextInt();
+
+            System.out.print("Enter Priority: ");
+            int priority = input.nextInt();
+
+            System.out.print("Enter Quantum Time: ");
+            int quantumTime = input.nextInt();
+
+            Process process =new Process(processName,arrivalTime,burstTime,priority,quantumTime,1);
+            processes.add(process);
+        }
+
     }
 }
