fc100b3c499f90cb2c6529f2fe127f7b9acc0ac2
rehamrashad <102626070+rehamrashad@users.noreply.github.com>
2022-12-09 19:40:17 +0200

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,43 +4,61 @@ import java.util.*;
 
 import AG.AG;
 import Process.Process;
+import SJF.SJF;
+
 public class Main {
 
 
     static Scanner input=new Scanner(System.in);
     public static void main(String[] args) {
         ArrayList<Process> processes=new ArrayList<Process>();
+        System.out.print("Enter process name: ");
+        String s = input.nextLine();
         System.out.print("Enter Number Of Process: ");
         int n = input.nextInt();
-        System.out.print("Enter Round robin Time Quantum: ");
-        int q = input.nextInt();
-        System.out.print("Enter Context switching: ");
-        int context = input.nextInt();
-        for (int i = 0; i < n; i++) {
-            System.out.println("For P" + (i+1) );
-
-            System.out.print("Enter process Name: ");
-            String processName = input.next();
-
-            System.out.print("Enter Burst Time: ");
-            int burstTime = input.nextInt();
-
-            System.out.print("Enter Arrival Time: ");
-            int arrivalTime = input.nextInt();
+        System.out.print("Enter context switching: ");
+        long c = input.nextInt();
 
 
-            System.out.print("Enter Priority: ");
-            int priority = input.nextInt();
-
-            System.out.print("Enter Quantum Time: ");
-            int quantumTime = input.nextInt();
-
-            Process process =new Process(processName,arrivalTime,burstTime,priority,quantumTime,1);
-            processes.add(process);
+        for (int i = 0; i <n ; i++) {
+            System.out.print("Enter arrival: ");
+            int x = input.nextInt();
+            System.out.print("Enter busrt: ");
+            int y = input.nextInt();
+            Process p=new Process(s,x,y,c);
+            processes.add(p);
         }
-        AG ag=new AG(processes);
-        ag.startProcess();
-        ag.show();
+        SJF sjf =new SJF(processes) ;
+        sjf.ans();
+//        System.out.print("Enter Round robin Time Quantum: ");
+//        int q = input.nextInt();
+//        System.out.print("Enter Context switching: ");
+//        int context = input.nextInt();
+//        for (int i = 0; i < n; i++) {
+//            System.out.println("For P" + (i+1) );
+//
+//            System.out.print("Enter process Name: ");
+//            String processName = input.next();
+//
+//            System.out.print("Enter Burst Time: ");
+//            int burstTime = input.nextInt();
+//
+//            System.out.print("Enter Arrival Time: ");
+//            int arrivalTime = input.nextInt();
+//
+//
+//            System.out.print("Enter Priority: ");
+//            int priority = input.nextInt();
+//
+//            System.out.print("Enter Quantum Time: ");
+//            int quantumTime = input.nextInt();
+//
+//            Process process =new Process(processName,arrivalTime,burstTime,priority,quantumTime,1);
+//            processes.add(process);
+//        }
+//        AG ag=new AG(processes);
+//        ag.startProcess();
+//        ag.show();
 
     }
 }
